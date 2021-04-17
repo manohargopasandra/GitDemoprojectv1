2a28ebd52e36957e45efc631ddac260056ae219a
manoh <manoh@192.168.2.13>
2021-04-17 18:25:56 -0400

    my first committ...

---
+++ src/Manugit/Gittest1.java
@@ -0,0 +1,14 @@
+package Manugit;
+
+public class Gittest1 {
+	public static void main(String[] args) {
+		
+		String Workhard= "yes";
+		
+		if (Workhard=="yes")
+			System.out.println("Eat something");
+		else 
+		System.out.println("Do wonders");
+	}
+
+}
